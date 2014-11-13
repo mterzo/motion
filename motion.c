16d4b95d957e736a8ff99a08cdc976480b75cbc9
Mike Terzo <mike@terzo.org>
2014-11-13 02:19:26 -0500

    Switched arguments to calloc as per manpage it's # of elements * sizeof

--- motion.c
+++ motion.c
@@ -2847,7 +2847,7 @@ int main (int argc, char **argv)
  */
 void * mymalloc(size_t nbytes)
 {
-    void *dummy = calloc(nbytes, 1);
+    void *dummy = calloc(1, nbytes);
 
     if (!dummy) {
         MOTION_LOG(EMG, TYPE_ALL, SHOW_ERRNO, "%s: Could not allocate %llu bytes of memory!",
